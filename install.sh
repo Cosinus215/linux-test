b06a3644b83a9ab95800cbe1bf1909b360b4e137
Cosinus215 <Cosinus215@gmail.com>
2026-06-10 05:23:43 +0300

    Update

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 function start() {
   #update;
   #install_core_tools;
+  install_qtile;
   copy_files;
 }
 
@@ -16,7 +17,17 @@ function install_core_tools() {
     xorg-x11-server dbus-1-x11 xinit xauth xterm \
     xf86-video-amdgpu xf86-video-intel Mesa-dri \
     dolphin konsole kate \
-    qtile feh rofi;
+    python3-pip python3-setuptools python3-dbus-python \
+    feh rofi cairo pango;
+}
+
+function install_qtile() {
+  # Qtile from OBS X11:WindowManagers (system package, not pip)
+  local leapver="15.6"  # change to your Leap version
+  sudo zypper addrepo \
+    "https://download.opensuse.org/repositories/X11:WindowManagers/openSUSE_Leap_$leapver/X11:WindowManagers.repo";
+  sudo zypper --gpg-auto-import-keys refresh;
+  sudo zypper install -y qtile;
 }
 
 function update() {
